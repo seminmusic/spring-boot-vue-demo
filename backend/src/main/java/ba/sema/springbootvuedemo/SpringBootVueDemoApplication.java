d2363ac00f66cd520ec6cb484c9e2ca3c774ac44
Semin Musić <music.semin@gmail.com>
2020-06-02 15:18:20 +0200

    Setup backend and frontend

---
+++ backend/src/main/java/ba/sema/springbootvuedemo/SpringBootVueDemoApplication.java
@@ -0,0 +1,21 @@
+package ba.sema.springbootvuedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootVueDemoApplication extends SpringBootServletInitializer
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootVueDemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(SpringBootVueDemoApplication.class);
+    }
+}
---
+++ frontend/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  lintOnSave: false,
+  outputDir: "target/dist",
+  assetsDir: "static"
+}
