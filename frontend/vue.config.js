4059babc296771a095f53482cc3562e323978341
Semin Musić <music.semin@gmail.com>
2020-06-03 11:33:09 +0200

    Setup ports and path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   lintOnSave: false,
   outputDir: "target/dist",
   assetsDir: "static",
-  publicPath: "/spring-and-vue/"
+  publicPath: "/spring-and-vue/",
+  devServer: {
+    port: 3001
+  }
 }
