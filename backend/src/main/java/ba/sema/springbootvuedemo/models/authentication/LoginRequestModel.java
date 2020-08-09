5639425aa1e59e6d0e8361c851e970ff564d77cb
Semin Musić <music.semin@gmail.com>
2020-08-09 19:05:43 +0200

    Fix javax.validation.constraints.NotBlank on WildFly

--- backend/src/main/java/ba/sema/springbootvuedemo/models/authentication/LoginRequestModel.java
+++ backend/src/main/java/ba/sema/springbootvuedemo/models/authentication/LoginRequestModel.java
@@ -2,14 +2,22 @@ package ba.sema.springbootvuedemo.models.authentication;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+// import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class LoginRequestModel
 {
-    @NotBlank
+    // @NotBlank - not working on WildFly, more info how to resolve this:
+    // https://stackoverflow.com/questions/49706795/notblank-not-recognised-in-wildfly-hibernate
+    // https://in.relation.to/2017/04/04/testing-bean-validation-2-0-on-wildfly-10/
+
+    @NotNull
+    @Size(min = 1)
     private String username;
 
-    @NotBlank
+    @NotNull
+    @Size(min = 1)
     private String password;
 }
--- backend/src/main/java/ba/sema/springbootvuedemo/models/authentication/RegisterUserRequestModel.java
+++ backend/src/main/java/ba/sema/springbootvuedemo/models/authentication/RegisterUserRequestModel.java
@@ -3,23 +3,23 @@ package ba.sema.springbootvuedemo.models.authentication;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class RegisterUserRequestModel
 {
-    @NotBlank
+    @NotNull
     @Size(max = 50)
     @Email
     private String email;
 
-    @NotBlank
+    @NotNull
     @Size(min = 3, max = 30)
     private String username;
 
-    @NotBlank
+    @NotNull
     @Size(min = 6, max = 40)
     private String password;
 
